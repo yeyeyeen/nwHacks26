dd307612b48c69f221d65b19601717691c193782
mikuro39 <121419505+mikuro39@users.noreply.github.com>
2026-01-17 15:04:06 -0800

    Separate backend into files

--- backend/app/controller/main_controller.py
+++ backend/app/controller/main_controller.py
@@ -2,9 +2,11 @@ from fastapi import HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from app import app
+from app.controller import feedback
 
 @app.get("/")
 def root():
     """Root endpoint"""
     return {"message": "Simple FastAPI REST Controller"}
 
+app.include_router(feedback.router)
